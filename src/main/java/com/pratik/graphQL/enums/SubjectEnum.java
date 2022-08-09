cca8d9fa20d8f38de0116ab856b0f9bc9b939c14
patcyy <pratikchatterjee2606@gmail.com>
2022-08-09 09:57:41 +0530

    Filter on edge Implementation

---
+++ src/main/java/com/pratik/graphQL/enums/SubjectEnum.java
@@ -0,0 +1,9 @@
+package com.pratik.graphQL.enums;
+
+public enum SubjectEnum {
+	All,
+	Math,
+	Power_System,
+	Electrical_Machine
+
+}
--- src/main/java/com/pratik/graphQL/resolver/StudentResponseResolver.java
+++ src/main/java/com/pratik/graphQL/resolver/StudentResponseResolver.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import com.coxautodev.graphql.tools.GraphQLResolver;
+import com.pratik.graphQL.enums.SubjectEnum;
 import com.pratik.graphQL.response.StudentResponse;
 import com.pratik.graphQL.response.SubjectResponse;
 
@@ -16,12 +17,18 @@ public class StudentResponseResolver implements GraphQLResolver<StudentResponse>
 
 	private static final Logger log = LoggerFactory.getLogger(StudentResponseResolver.class);
 
-	public List<SubjectResponse> getSubjects(StudentResponse studentResponse) {
+	public List<SubjectResponse> getSubjects(StudentResponse studentResponse, SubjectEnum subjectEnum) {
 
 		log.info("Query Resolver Invoked for Student : {}", studentResponse.getStudentId());
 
 		List<SubjectResponse> subjects = new ArrayList<>();
-		studentResponse.getStudent().getSubjects().forEach(e -> subjects.add(new SubjectResponse(e)));
+		//WIthout filter on edge
+		//studentResponse.getStudent().getSubjects().forEach(e -> subjects.add(new SubjectResponse(e)));
+		
+		//Implementing filter on edge
+		studentResponse.getStudent().getSubjects().stream()
+				.filter(e -> subjectEnum.name().equalsIgnoreCase("All")||subjectEnum.name().equalsIgnoreCase(e.getSubjectName()))
+				.forEach(e -> subjects.add(new SubjectResponse(e)));
 		return subjects;
 
 	}
